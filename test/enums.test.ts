24faa2865271f26b2bb8edc207b242308c4b0682
Franklin Davenport <sparebytes@gmail.com>
2019-06-19 14:34:16 -0500

    tests: add test for enums

---
+++ test/enums.test.ts
@@ -0,0 +1,20 @@
+import test from "ava";
+import { assertTypeFn } from "../dist";
+
+enum Foo {
+  bar,
+  baz,
+  biz,
+}
+
+const assertFoo = assertTypeFn<Foo>();
+
+test("Enums Valid", t => {
+  t.assert(assertFoo(Foo.bar).unwrap() === Foo.bar);
+  t.assert(assertFoo(Foo.baz).unwrap() === Foo.baz);
+  t.assert(assertFoo(Foo.biz).unwrap() === Foo.biz);
+});
+
+test("Enums Invalid", t => {
+  t.throws(() => assertFoo(-1).unwrap());
+});
