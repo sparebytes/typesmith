e60de897d9b00462746adafb21eeccbfe4fa285b
Franklin Davenport <sparebytes@gmail.com>
2019-06-20 09:42:06 -0500

    test: write buffer test

---
+++ test/issues/buffer.test.ts
@@ -0,0 +1,23 @@
+import test from "ava";
+import { assertTypeFn } from "../../dist";
+
+export class DataBox {
+  data?: string | Buffer;
+}
+
+/**
+ * This will not tranform due to the `data: Buffer` property.
+ */
+export const assertDataBox = assertTypeFn<DataBox>();
+
+test("Buffer", t => {
+  t.assert(assertDataBox({ data: null }).unwrap().data === null);
+  t.assert(assertDataBox({ data: "a" }).unwrap().data === "a");
+  t.assert(
+    assertDataBox({ data: Buffer.from("b") })
+      .unwrap()
+      .data.toString() === "b",
+  );
+  t.throws(() => assertDataBox({ data: 1 }).unwrap());
+  t.throws(() => assertDataBox({ data: {} }).unwrap());
+});
