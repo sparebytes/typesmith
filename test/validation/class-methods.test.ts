5397e60384e6340ac07303e2306f92c4505a3627
Franklin Davenport <sparebytes@gmail.com>
2019-06-25 23:38:44 -0500

    test: add class-methods test

---
+++ test/validation/class-methods.test.ts
@@ -0,0 +1,15 @@
+import test from "ava";
+import { assertTypeFn } from "../../dist";
+
+test("Class Methods", t => {
+  class MyClass {
+    data: any;
+    doSomething(v: number): string {
+      return "string";
+    }
+  }
+
+  const assertMyClass = assertTypeFn<MyClass>();
+
+  t.assert(assertMyClass({ data: 123 }).unwrap().data === 123);
+});
