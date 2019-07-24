3cdea154f6376422c293d160d506e301b46a405d
Franklin Davenport <sparebytes@gmail.com>
2019-07-24 15:28:10 -0500

    enhancement: improve performance by lazily initializing ajv

--- src/assert-type-fn-factory.ts
+++ src/assert-type-fn-factory.ts
@@ -4,19 +4,25 @@ import ajvErrorsPlugin = require("ajv-errors");
 import { AssertTypeFailure, AssertTypeFn, AssertTypeOptions, AssertTypeSuccess } from "./assert-types";
 import { settings } from "./settings";
 
-export function assertTypeFnFactory<T>(options: AssertTypeOptions, jsonSchema: any): AssertTypeFn<T> {
+function makeAvj(options: AssertTypeOptions) {
   const ajv = new Ajv({
     ...settings.useGlobalValidationOptions(),
     ...options,
     jsonPointers: true, // jsonPointers must be true for ajv-errors
   } as Ajv.Options);
   ajvErrorsPlugin(ajv);
+  return ajv;
+}
+
+export function assertTypeFnFactory<T>(options: AssertTypeOptions, jsonSchema: any): AssertTypeFn<T> {
   let typeValidateFn: Ajv.ValidateFunction;
 
   if (options.lazyCompile === false) {
+    const ajv = makeAvj(options);
     typeValidateFn = ajv.compile(jsonSchema);
   } else {
     typeValidateFn = (...args: any[]) => {
+      const ajv = makeAvj(options);
       typeValidateFn = ajv.compile(jsonSchema);
       return typeValidateFn.apply(null, args as any);
     };
