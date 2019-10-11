c6e800fb41e27889301ef72a11838f637ba4a8f2
Franklin Davenport <sparebytes@gmail.com>
2019-10-10 23:32:09 -0500

    test(coerceTypes): add additional tests for coercing nullable properties

--- test/validation/coerce.test.ts
+++ test/validation/coerce.test.ts
@@ -1,11 +1,34 @@
 import test from "ava";
 import { assertTypeFn, Int } from "../../dist";
 
-test("Coerce", t => {
-  interface MyBox {
-    value?: Int | null;
-  }
+interface MyBox {
+  value: Int | null;
+}
+
+test("coerceTypes: true", t => {
   const assertMyBox = assertTypeFn<MyBox>({ coerceTypes: true });
 
   t.assert(assertMyBox({ value: null }).unwrap().value === null);
+  t.assert(assertMyBox({ value: 1 }).unwrap().value === 1);
+  t.assert(assertMyBox({ value: "1" }).unwrap().value === 1);
+  t.assert(assertMyBox({ value: "" }).unwrap().value === null);
+  t.throws(() => assertMyBox({ value: NaN }).unwrap());
+  t.throws(() => assertMyBox({ value: Infinity }).unwrap());
+  t.throws(() => assertMyBox({ value: -Infinity }).unwrap());
+  t.throws(() => assertMyBox({ value: 1.1 }).unwrap());
+  t.throws(() => assertMyBox({ value: "1.1" }).unwrap());
+});
+
+test("coerceTypes: false", t => {
+  const assertMyBox = assertTypeFn<MyBox>({ coerceTypes: false });
+
+  t.assert(assertMyBox({ value: null }).unwrap().value === null);
+  t.assert(assertMyBox({ value: 1 }).unwrap().value === 1);
+  t.throws(() => assertMyBox({ value: "1" }).unwrap());
+  t.throws(() => assertMyBox({ value: "" }).unwrap());
+  t.throws(() => assertMyBox({ value: NaN }).unwrap());
+  t.throws(() => assertMyBox({ value: Infinity }).unwrap());
+  t.throws(() => assertMyBox({ value: -Infinity }).unwrap());
+  t.throws(() => assertMyBox({ value: 1.1 }).unwrap());
+  t.throws(() => assertMyBox({ value: "1.1" }).unwrap());
 });
